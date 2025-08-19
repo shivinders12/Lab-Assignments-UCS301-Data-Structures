e9a6bdc1fb58de19888f74341b0ffea12c736fd7
shivinders12 <singhshivinder.12@gmail.com>
2025-08-19 22:37:12 +0530

    1

--- Assignment 2/3.cpp
+++ Assignment 2/3.cpp
@@ -9,18 +9,14 @@ int main() {
     int arr[5]={1,2,3,5,6};
     int n= 5;
 
-    for(int i=0;i<n;i++)
-    {
-        if(arr[i]!=i+1)
-        {
-             cout<<i;
-            break;
-           
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i+1] - arr[i] != 1) {
+            cout << "Missing number: " << arr[i] + 1;
+            break; 
         }
-        
-
-        
     }
+
+    
 
     return 0;
 }
